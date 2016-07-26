f08afa8c5036a0aff1c23265d9c2f56552c1cbd3
byeh <byeh@boingo.com>
2016-07-26 01:18:02 -0700

    fixes to library

---
+++ matrix/matrix.py
@@ -0,0 +1,44 @@
+class Matrix(object):
+    def __init__(self, list_of_rows):
+        self.matrix = list_of_rows
+
+    def rows(self):
+        return len(self.matrix)
+
+    def cols(self):
+        return len(self.matrix[0])
+
+    def getRow(self, n):
+        return self.matrix[n]
+
+    def getCol(self, n):
+        return [[row[n]] for row in self.matrix]
+
+    def transpose(self):
+        return Matrix([[i[0] for i in self.getCol(col_index)] for col_index in range(self.cols())])
+
+    def __add__(self, other):
+        if self.cols() != other.cols() or self.rows() != other.rows():
+            raise ArithmeticError
+        return Matrix([[col0+col1 for col0, col1 in zip(row0,row1)] for row0, row1 in zip(self.matrix, other.matrix)])
+
+    def __neg__(self):
+        return Matrix([[-i for i in row] for row in self.matrix])
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __repr__(self):
+        return "\n".join([str(row) for row in self.matrix])
+
+    def __mul__(self, other):
+        if self.rows() != other.cols():
+            raise ArithmeticError
+        result = []
+        for row in self.matrix:
+            result_row = []
+            for col_index in range(other.cols()):
+                col = sum([i*j for i,j in zip(Matrix(other.getCol(col_index)).transpose().getRow(0),row)])
+                result_row.append(col)
+            result.append(result_row)
+        return Matrix(result)

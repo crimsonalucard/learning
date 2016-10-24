35679b5723a2f8f2a3b5ee5b36a75f46bc3df3eb
brian yeh <brian@brians-MacBook-Pro-2.local>
2016-10-23 17:26:00 -0700

    completed display and reading of mnist data

--- test.py
+++ test.py
@@ -3,6 +3,8 @@
 from neural_net.neural_net import create_neural_net, initialize_thetas, create_forward_propogator, create_backward_propogator
 from matrix.matrix import Matrix
 from misc.utils import sigmoid, sigmoid_derivative, square_error, square_error_derivative
+from display.display import show
+from mnist.reader import get_image_data, convert_flattened_array_to_matrix
 
 
 x = create_neural_net([[0,0,0],[0,0,0]], activation_function=sigmoid)
@@ -17,5 +19,13 @@ print(y(1,1))
 thetas = initialize_thetas(1, 784, 15, 10)
 print(thetas)
 
+print("testing data retrieval")
+images = get_image_data()
+test_image = convert_flattened_array_to_matrix(28,28,images[0])
+#import pdb; pdb.set_trace()
+show(test_image, 10, "testing!")
+
+"""
 print("testing backward propogator")
 d = create_backward_propogator(thetas, square_error, square_error_derivative, sigmoid, sigmoid_derivative)
+"""
